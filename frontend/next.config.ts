0711b6c42a006108c3dfcc535c7778d0e1f7a5d8
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2025-04-05 19:17:26 +0530

    web pack update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        child_process: false,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

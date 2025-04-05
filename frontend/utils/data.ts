28d12b95ce9f16800214c8f763daee4cfd027d34
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2025-04-05 14:25:17 +0530

    data init

---
+++ frontend/utils/data.ts
@@ -0,0 +1,29 @@
+const modeData = [
+  {
+    image: "",
+    title: "General",
+    redirect: "/general",
+  },
+  {
+    image: "",
+    title: "Relegion",
+    redirect: "/relegion",
+  },
+  {
+    image: "",
+    title: "Health",
+    redirect: "/health",
+  },
+  {
+    image: "",
+    title: "Schemes",
+    redirect: "/schemes",
+  },
+  {
+    image: "",
+    title: "Order",
+    redirect: "/order",
+  },
+];
+
+export { modeData };
